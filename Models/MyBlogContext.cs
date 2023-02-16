407b2ad6fbb922d344d4ea4d61254645088fffaf
Phuong Hoang <you@example.com>
2023-02-16 22:46:35 +0700

    Tich hop EF

---
+++ Models/MyBlogContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> dbContextOptions) : base(dbContextOptions)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Article> Articles { get; set; }    
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly MyBlogContext _myBlogContext;
+
+        public IndexModel(ILogger<IndexModel> logger, MyBlogContext myBlogContext)
+        {
+            _logger = logger;
+            _myBlogContext = myBlogContext;
+        }
+
+        public void OnGet()
+        {
+            var posts = (from article in _myBlogContext.Articles
+                        orderby article.Created descending
+                        select article).ToList();
+            ViewData["posts"] = posts;
+        }
+    }
+}
