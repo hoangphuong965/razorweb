c8ba856ed76e2924742277fe5e473f2d6a822d2e
Phuong Hoang <you@example.com>
2023-02-18 21:21:37 +0700

    Add Identity

--- Models/MyBlogContext.cs
+++ Models/MyBlogContext.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Models
 {
-    public class MyBlogContext : DbContext
+    public class MyBlogContext : IdentityDbContext<AppUser>
     {
         public MyBlogContext(DbContextOptions<MyBlogContext> dbContextOptions) : base(dbContextOptions)
-        { }
+        { 
+            
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -15,6 +18,17 @@ namespace WebApplication1.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+            // lay dc ten bang mà nó tạo ra
+                var tableName = entityType.GetTableName();
+                // loai  bo tiền tố AspNet khi tạo bảng
+                if(tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
         }
 
         public DbSet<Article> Articles { get; set; }    
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,10 @@
+﻿using Album.Mail;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,12 +29,52 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            var mailsetting = Configuration.GetSection("MailSettings");
+            services.Configure<MailSettings>(mailsetting);
+            services.AddSingleton<IEmailSender, SendMailService>();
+
             services.AddRazorPages();
             services.AddDbContext<MyBlogContext>(options =>
             {
                 // thiet lap chuoi ket noi khi khoi tao MyBlogContext
                 string connectString = Configuration.GetConnectionString("MyBlogContext");
                 options.UseSqlServer(connectString);
+            });
+
+            // Dang ky Identity
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<MyBlogContext>()
+                .AddDefaultTokenProviders();
+
+            // su dung giao dien mac dinh
+            //services.AddDefaultIdentity<AppUser>()
+            //    .AddEntityFrameworkStores<MyBlogContext>()
+            //.AddDefaultTokenProviders();
+
+            // Truy cập IdentityOptions
+            services.Configure<IdentityOptions>(options => {
+                // Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+                // Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+                options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+                // Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
             });
         }
 
@@ -53,16 +97,28 @@ namespace WebApplication1
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
             });
+
+
+
         }
     }
 }
 
 /*
  dotnet aspnet-codegenerator razorpage -m WebApplication1.Models.Article -dc WebApplication1.Models.MyBlogContext -outDir Pages/Blog -udl --referenceScriptLibraries
+
+ Identity:
+    - Authentication: Xác định danh tính -> login, logout, ...
+    - Authorization: Xác thực quyền truy cập
+    - Quản lý user: Sign Up, User, Role ...
+
+dotnet aspnet-codegenerator identity -dc WebApplication1.Models.MyBlogContext
+
  */
